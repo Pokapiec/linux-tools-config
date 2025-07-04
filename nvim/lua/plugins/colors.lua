f9d1969ad6742cbb3c1d7c303fbfcff1d1be68e1
Paweł <64706471+Pokapiec@users.noreply.github.com>
2025-07-04 21:07:48 +0200

    Add files via upload

---
+++ nvim/lua/plugins/colors.lua
@@ -0,0 +1,38 @@
+-- local function enable_transparency()
+--     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--     vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+-- end
+
+return {
+    -- {
+    --     "folke/tokyonight.nvim",
+    --     config = function()
+    --         vim.cmd.colorscheme "tokyonight"
+    --         vim.cmd('hi Directory guibg=NONE')
+    --         vim.cmd('hi SignColumn guibg=NONE')
+    --         enable_transparency()
+    --     end
+    -- },
+    -- {
+    --     "Mofiqul/vscode.nvim",
+    --     name = 'vscode',
+    --     config = function()
+    --         vim.cmd.colorscheme "vscode"
+    --         vim.cmd('hi Directory guibg=NONE')
+    --         vim.cmd('hi SignColumn guibg=NONE')
+    --         enable_transparency()
+    --     end
+    -- }
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                flavour = "mocha", -- or your choice
+            })
+            vim.cmd.colorscheme("catppuccin")
+        end,
+    }
+}
